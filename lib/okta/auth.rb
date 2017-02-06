6198166b3f0ffc116027c687ea9fb55c01613b45
A Arrow <oneone@gmail.com>
2017-02-06 18:44:40 +0000

    create

--- lib/okta/auth.rb
+++ lib/okta/auth.rb
@@ -4,5 +4,21 @@ module Okta
       data = {username: email, password: password}
       Okta.post("/authn", data)
     end
+
+    def self.create_user(fname, lname, email, phone, password, question, answer)
+      data = {"profile": {
+          "firstName": fname,
+          "lastName": lname,
+          "email": email,
+          "login": email,
+          "mobilePhone": phone
+        },
+        "credentials": {
+          "password" : { "value": password },
+          "recovery_question": { "question": question, "answer": answer }
+        }
+      }
+      Okta.post("/users?activate=true", data)
+    end
   end
 end
