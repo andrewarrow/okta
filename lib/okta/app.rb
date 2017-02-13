f5f9d1ccbb099110718aef3cfd7b87eb0ceedb44
A Arrow <oneone@gmail.com>
2017-02-13 19:58:16 +0000

    mm

--- lib/okta/app.rb
+++ lib/okta/app.rb
@@ -3,5 +3,11 @@ module Okta
     def self.find(id)
       Okta.get("/apps/#{id}")
     end
+    
+    # /api/v1/apps/:aid/users/:uid
+    def self.update(aid, uid, data)
+      data = {profile: {data}}
+      Okta.post("/apps/#{aid}/users/#{uid}", data)
+    end
   end
 end
